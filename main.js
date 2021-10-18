750de009f2475076a1b0cd73c249e60028bc647b
antoniowav <85946713+antoniowav@users.noreply.github.com>
2021-10-18 09:45:59 +0200

    fixed function addProductTo Webpage

--- main.js
+++ main.js
@@ -22,79 +22,77 @@ function initSite() {
 
 /** Uses the loaded products data to create a visible product list on the website */
 function addProductsToWebpage() {
-    // Check your console to see that the products are stored in the listOfProducts varible.
-    listOfProducts.forEach (function (product) {
-        
-        //declare the maincontainer for all the content on page
-        let mainContainer = document.getElementById('mainContainer');
-        // create div-container for each phone
-        let productContainer = document.createElement('div');
-        productContainer.classList.add('productContainer');
-        
 
-            //create img-container and IMG-tag
+    createUI();
 
-            let imgContainer = document.createElement('div');
-            imgContainer.classList.add('imgContainer');
-            let productImg = document.createElement('img');   
-            productImg.src = imgUrl + product.image;
-            imgContainer.appendChild (productImg);
-
-            //ASK VICTOR ABOUT THIS
-
-         /*    let productImg2 = document.createElement('img');   
-            productImg2.src = oneplus + product.image;
-            imgContainer.appendChild (productImg2); */
-
-
-
-
-        // create div-text descripton for each phone
-        let textContainer = document.createElement('div');
-        textContainer.classList.add ('textContainer');
-
-        let productHeader = document.createElement('H1')
-        let productText = document.createElement('p');
-        
-
-        productHeader.innerText = product.title;
-        productText.innerText = product.description;
-
-
-
-        //create price div and p-tag
-
-        let priceContainer = document.createElement('div');
-        priceContainer.classList.add ('priceContainer');
-        priceText = document.createElement ('p');
-        priceContainer.appendChild (priceText);
-        priceText.innerText = product.price + ' kr';
-
-        // create button div and tag
-        let btnContainer = document.createElement('div');
-        let clickButton = document.createElement('div');
-        let cartIcon = document.createElement('i');
-        cartIcon.classList.add('fas', 'fa-cart-arrow-down');
-        
-        let addToCartBtn = document.createElement('a');
-        let btnText = document.createTextNode ('Lägg till i kundvagn');
-        addToCartBtn.setAttribute ('href', 'http://www.google.com/');
-        btnContainer.classList.add ('btnContainer');
-        addToCartBtn.appendChild(cartIcon)
-        addToCartBtn.appendChild(clickButton)
-        btnContainer.appendChild(addToCartBtn);
-        addToCartBtn.append (btnText);
-        
-        // Appending everything ---->
-        mainContainer.appendChild(productContainer);
-        productContainer.append(textContainer, imgContainer, priceContainer, btnContainer );
-        textContainer.append (productHeader, productText);
-
-    })
-
+    // Check your console to see that the products are stored in the listOfProducts varible.   
     // Add your code here, remember to brake your code in to smaller function blocks
     // to reduce complexity and increase readability. Each function should have
     // an explainetory comment like the one for this function, see row 22.
     
     // TODO: Remove the console.log and these comments when you've read them.
+}
+
+
+function createUI() {
+
+    listOfProducts.forEach (function (product) {
+        
+    //declare the maincontainer for all the content on page
+    let mainContainer = document.getElementById('mainContainer');
+
+    // create div-container for each phone
+    let productContainer = document.createElement('div');
+    productContainer.classList.add('productContainer');
+    
+
+    //create img-container and IMG-tag
+    let imgContainer = document.createElement('div');
+    imgContainer.classList.add('imgContainer');
+    let productImg = document.createElement('img');   
+    productImg.src = imgUrl + product.image;
+    imgContainer.appendChild (productImg);
+
+
+    // create div-text descripton for each phone
+    let textContainer = document.createElement('div');
+    textContainer.classList.add ('textContainer');
+
+    let productHeader = document.createElement('H1')
+    let productText = document.createElement('p');
+    
+
+    productHeader.innerText = product.title;
+    productText.innerText = product.description;
+
+
+
+    //create price div and p-tag
+    let priceContainer = document.createElement('div');
+    priceContainer.classList.add ('priceContainer');
+    priceText = document.createElement ('p');
+    priceContainer.appendChild (priceText);
+    priceText.innerText = product.price + ' kr';
+
+    // create button div and tag
+    let btnContainer = document.createElement('div');
+    let clickButton = document.createElement('div');
+    let cartIcon = document.createElement('i');
+    cartIcon.classList.add('fas', 'fa-cart-arrow-down');
+    
+    let addToCartBtn = document.createElement('a');
+    let btnText = document.createTextNode ('Lägg till i kundvagn');
+    addToCartBtn.setAttribute ('href', 'http://www.google.com/');
+    btnContainer.classList.add ('btnContainer');
+    addToCartBtn.appendChild(cartIcon)
+    addToCartBtn.appendChild(clickButton)
+    btnContainer.appendChild(addToCartBtn);
+    addToCartBtn.append (btnText);
+    
+    // Appending everything ---->
+    mainContainer.appendChild(productContainer);
+    productContainer.append(textContainer, imgContainer, priceContainer, btnContainer );
+    textContainer.append (productHeader, productText);
+
+})
 }
