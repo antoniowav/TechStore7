16d7a71193df0db3802345921e7ff8a407862d58
antoniowav <85946713+antoniowav@users.noreply.github.com>
2021-11-01 23:30:39 +0100

    added ta bort button and functions in it

--- cart.js
+++ cart.js
@@ -1,168 +1,206 @@
-const imgUrl = '/assets/';
+const imgUrl = "/assets/";
 function initSite() {
-    addProductsToWebpage();
-    // This would also be a good place to initialize other parts of the UI
+  addProductsToWebpage();
+
+  // This would also be a good place to initialize other parts of the UI
 }
 
 /** Uses the loaded products data to create a visible product list on the website */
 function addProductsToWebpage() {
+  let mainContainer = document.getElementById("mainContainer");
+  let basket = document.querySelectorAll(".btnContainer");
 
-    let mainContainer = document.getElementById('mainContainer');
-    let basket = document.querySelectorAll('.btnContainer');
+ 
+
+  for (let i = 0; i < basket.length; i++) {
+    basket[i].addEventListener("click", () => {
+      cartNumbers(listOfProducts[i]);
+      totalCost(listOfProducts[i]);
+    });
+  }
+
+  // Add to cart function
+
+  function onLoadCartNumbers() {
+    let productNumber = localStorage.getItem("cartNumbers");
+
+    if (productNumber) {
+      document.querySelector(".itemsNumber, span").textContent = productNumber;
+    }
+  }
+
+  function cartNumbers(product) {
+    let productNumber = localStorage.getItem("cartNumbers");
+    productNumber = parseInt(productNumber);
+
+    if (productNumber) {
+      localStorage.setItem("cartNumbers", productNumber + 1);
+      document.querySelector(".itemsNumber, span").textContent =
+        productNumber + 1;
+    } else {
+      localStorage.setItem("cartNumbers", 1);
+      document.querySelector(".itemsNumber, span").textContent = 1;
+    }
+    setItems(product);
+  }
+
+  // skapar elements till cart.html
+  function createCartSection() {
+    const totalCost = localStorage.getItem("totalCost");
+    const cartNumbers = localStorage.getItem("cartNumbers");
+
+    const quantityContainer = document.createElement("div");
+    quantityContainer.classList.add("quantity");
+    const cartSection = document.createElement("div");
+    cartSection.classList.add("cartContainer");
+    const quantityBox = document.createElement("div");
+    quantityBox.classList.add("quantityContainer");
+    quantityBox.append(quantityContainer);
+    const totalPriceBox = document.createElement("div");
+    totalPriceBox.classList.add("priceContainer");
+    const totalPriceContainer = document.createElement("div");
+    totalPriceContainer.classList.add(".price");
+    totalPriceBox.append(totalPriceContainer);
+    const img = document.createElement("div");
+    img.classList.add("imgContainer");
+
+    // Skapar "slutför köp" knappen
+
+    const checkoutBtnFlex = document.createElement("div");
+    checkoutBtnFlex.classList.add("checkoutBtnFlex");
+    const checkOutBtnContainer = document.createElement("div");
+    checkOutBtnContainer.classList.add("checkOutBtnContainer");
+    const checkOutBtnText = document.createElement("span");
+    checkOutBtnText.innerText = "Slutför ditt köp";
+    const checkoutBtnIcon = document.createElement("i");
+    checkoutBtnIcon.classList.add("fas", "fa-check");
+
+    checkOutBtnContainer.append(checkoutBtnIcon, checkOutBtnText);
+    checkoutBtnFlex.append(checkOutBtnContainer);
+
+    let products = localStorage.getItem("productsInCart");
+
+    // skapar cart-symbol
+    let cartIcon = document.createElement("i");
+    cartIcon.classList.add("fas", "fa-shopping-cart");
+
+    // Skapar Kundvagn-rubrik texten
+    let cartHeaderContainer = document.createElement("div");
+    cartHeaderContainer.classList.add("cartHeaderContainer");
+    let cartHeader = document.createElement("h1");
+    cartHeader.innerText = "Kundvagn";
+
+    cartHeaderContainer.append(cartIcon, cartHeader);
+    mainContainer.append(cartHeaderContainer);
+
+    // Detta if-statement körs bara om kundvagen är TOM!
+    if (products == null) {
+        
+      let basketEmpty = document.createElement("h3");
+      basketEmpty.innerText = "Kundvagnen är tom!";
+
+      let basketEmptyContainer = document.createElement("div");
+      basketEmptyContainer.classList.add("message");
+
+      basketEmptyContainer.append(basketEmpty);
+      mainContainer.append(basketEmptyContainer);
+      
+    } else {
+      quantityBox.append(`Antal: ${cartNumbers}`);
+      totalPriceBox.append(`Totalt pris: ${totalCost} kr`);
+      mainContainer.append(
+        cartSection,
+        totalPriceBox,
+        quantityBox,
+        checkoutBtnFlex
+      );
+
+      products = JSON.parse(products);
+      for (var i = 0; i < products.length; i++) {
+        const productCard = document.createElement("div");
+        const title = document.createElement("h1");
+        const imgDisp = document.createElement("img");
+        const imgContainer = document.createElement("div");
+        imgDisp.setAttribute("src", `${imgUrl}/${products[i].image}`);
+        imgDisp.setAttribute("alt", `${products[i].title}`);
+        const productPrice = document.createElement("h6");
+        const productQuantity = document.createElement("h5");
+
+
+        const deleteProductBtn = document.createElement('div');
+        deleteProductBtn.classList.add("deleteProductBtn");
+        const deleteProductBtnText = document.createElement("span");
+        deleteProductBtnText.innerText = "Ta bort";
+        const deleteProductBtnIcon = document.createElement("i");
+        deleteProductBtnIcon.classList.add("far", "fa-trash-alt");
+
+        deleteProductBtn.append(deleteProductBtnIcon, deleteProductBtnText);
+
+        productPrice.classList.add("productPrice");
+        productQuantity.classList.add("productQuantity");
+        productCard.classList.add("productCard");
+        imgContainer.classList.add("imgContainer");
+
+        productPrice.append(products[i].price + " kr");
+        productQuantity.append("Antal: " + products[i].quantity);
+        title.append(products[i].title);
+        cartSection.appendChild(productCard);
+        imgContainer.append(imgDisp);
+        productCard.append(imgContainer, title, productPrice, productQuantity, deleteProductBtn);
+      }
+    }
+
+  }
+
+  createCartSection();
+  onLoadCartNumbers();
+  
+  let deleteButton = document.querySelectorAll(".deleteProductBtn");
+
+  for (let i = 0; i < deleteButton.length; i++) {
+    let data = JSON.parse(localStorage.getItem('productsInCart')); 
+     
+    deleteButton[i].addEventListener("click", () => {
+        removeItems(i, data[i]);
+        console.log('hello');
+     
+    });
+  }
+
+  function removeItems(index, product) {
+      
+      console.log(index, product);
+      let data = localStorage.getItem('productsInCart');
+      let cartQuantity = localStorage.getItem('cartNumbers');
+      let totalPrice = localStorage.getItem('totalCost');
+      data = JSON.parse(data);
+      if(product.quantity === 1){
+          data.splice(index, 1)
+      }else{
+          data[index].quantity -= 1;
+      }
+      localStorage.removeItem('productsInCart');
+      localStorage.setItem('productsInCart', JSON.stringify(data));
+      localStorage.setItem('cartNumbers', cartQuantity -1);
+      localStorage.setItem('totalCost', totalPrice - product.price);
+      hideBtn();
+      location.reload();
+      console.log(data);
+    }
+
+    function hideBtn(){
+        let quantityLeft = localStorage.getItem('cartNumbers');
+        quantityLeft = JSON.parse(quantityLeft)
+        if(quantityLeft === 0){
+            localStorage.clear()
+        }
+    }
+        
+        
+    }
 
     
-
-  /*   listOfProducts.push('inCart', 0); */
-
-    for (let i=0; i < basket.length; i++) {
-        basket[i].addEventListener('click', () => {
-            cartNumbers(listOfProducts[i]);
-            totalCost(listOfProducts[i])
-        }) 
-    }
+    
 
 
 
-
-   // Add to cart function
-
-    function onLoadCartNumbers() {
-        let productNumber = localStorage.getItem('cartNumbers');
-
-        if(productNumber) {
-            document.querySelector('.itemsNumber, span').textContent = productNumber;
-        }
-    }
-
-   function cartNumbers(product) {
-       let productNumber = localStorage.getItem('cartNumbers');
-       productNumber = parseInt(productNumber);
-
-       if(productNumber) {
-        
-            localStorage.setItem('cartNumbers', productNumber + 1);
-            document.querySelector('.itemsNumber, span').textContent = productNumber + 1;
-        } else {
-            localStorage.setItem('cartNumbers', 1);
-            document.querySelector('.itemsNumber, span').textContent = 1;
-
-        }
-        setItems(product);
-       
-    }
-    // skapar elements till cart.html
-    function createCartSection(){
-        const totalCost = localStorage.getItem('totalCost')
-        const cartNumbers = localStorage.getItem('cartNumbers')
-        
-        
-        const quantityContainer = document.createElement('div')
-        quantityContainer.classList.add('quantity')
-        const cartSection = document.createElement('div');
-        cartSection.classList.add('cartContainer')
-        const quantityBox = document.createElement('div')
-        quantityBox.classList.add('quantityContainer')
-        quantityBox.append(quantityContainer)
-        const totalPriceBox = document.createElement('div')
-        totalPriceBox.classList.add('priceContainer')
-        const totalPriceContainer = document.createElement('div')
-        totalPriceContainer.classList.add('.price')
-        totalPriceBox.append(totalPriceContainer)
-        const img = document.createElement('div')
-        img.classList.add('imgContainer')
-
-        // Skapar "slutför köp" knappen
-
-        const checkoutBtnFlex = document.createElement('div')
-        checkoutBtnFlex.classList.add('checkoutBtnFlex')
-        const checkOutBtnContainer = document.createElement('div');
-        checkOutBtnContainer.classList.add('checkOutBtnContainer');
-        const checkOutBtnText = document.createElement('span');
-        checkOutBtnText.innerText = ('Slutför ditt köp');
-        const checkoutBtnIcon = document.createElement('i');
-        checkoutBtnIcon.classList.add('fas', 'fa-check');
-
-        checkOutBtnContainer.append(checkoutBtnIcon, checkOutBtnText);
-        checkoutBtnFlex.append(checkOutBtnContainer);
-       
-
-        
-        
-        
-        
-        let products = localStorage.getItem('productsInCart')
-
-        // skapar cart-symbol
-        let cartIcon = document.createElement('i');
-        cartIcon.classList.add('fas', 'fa-shopping-cart');
-        
-        // Skapar Kundvagn-rubrik texten
-        let cartHeaderContainer = document.createElement('div');
-        cartHeaderContainer.classList.add('cartHeaderContainer')
-        let cartHeader = document.createElement('h1');
-        cartHeader.innerText=('Kundvagn');
-
-        cartHeaderContainer.append(cartIcon, cartHeader);
-        mainContainer.append(cartHeaderContainer);
-
-
-        
-        // Detta if-statement körs bara om kundvagen är TOM!
-        if (products == null){
-            let basketEmpty = document.createElement('h3');
-            basketEmpty.innerText=('Kundvagnen är tom!')
-
-            let basketEmptyContainer = document.createElement('div');
-            basketEmptyContainer.classList.add('message')
-
-            basketEmptyContainer.append(basketEmpty);
-            mainContainer.append(basketEmptyContainer); 
-
-       } else {
-
-        quantityBox.append(`Antal: ${cartNumbers}`)
-        totalPriceBox.append(`Totalt pris: ${totalCost} kr`)
-        mainContainer.append(cartSection, totalPriceBox, quantityBox, checkoutBtnFlex);
-       
-        products = JSON.parse(products)
-        for (var i = 0; i < products.length; i++){
-            const productCard = document.createElement('div');
-            const title = document.createElement('h1')
-            const imgDisp = document.createElement('img')
-            const imgContainer = document.createElement('div');
-            imgDisp.setAttribute("src", `${imgUrl}/${products[i].image}`)
-            imgDisp.setAttribute("alt", `${products[i].title}` )
-            const productPrice = document.createElement('h6')
-            const productQuantity = document.createElement('h5')
-
-
-            productPrice.classList.add('productPrice')
-            productQuantity.classList.add('productQuantity')
-            productCard.classList.add('productCard')
-            imgContainer.classList.add('imgContainer')
-            
-
-            
-            productPrice.append(products[i].price + ' kr')
-            productQuantity.append('Antal: '+ products[i].quantity)
-            title.append(products[i].title)
-            cartSection.appendChild(productCard);
-            imgContainer.append(imgDisp);
-            productCard.append( imgContainer, title, productPrice , productQuantity )
-
-        
-        
-        }
-
-    }
-
-       
-
-        
-       
-    }
-
-    createCartSection();
-    onLoadCartNumbers();
-    // createProductCard();
-}
--- main.js
+++ main.js
@@ -1,193 +1,167 @@
 var listOfProducts;
-const imgUrl = '/assets/';
-let cartArray = []
+const imgUrl = "/assets/";
+let cartArray = [];
 /** Get products from the json file and store it in a gobal variable */
 function loadProducts() {
-    fetch("./products.json")
-    .then(function(response) {
-        return response.json();
+  fetch("./products.json")
+    .then(function (response) {
+      return response.json();
     })
-    .then(function(products) {
-        listOfProducts = products;
-        addProductsToWebpage();
+    .then(function (products) {
+      listOfProducts = products;
+      addProductsToWebpage();
     });
-    
 }
 
-
 function initSite() {
-    loadProducts();
-    // This would also be a good place to initialize other parts of the UI
+  loadProducts();
+  // This would also be a good place to initialize other parts of the UI
 }
 
 /** Uses the loaded products data to create a visible product list on the website */
 function addProductsToWebpage() {
+  let mainContainer = document.getElementById("mainContainer");
 
-    let mainContainer = document.getElementById('mainContainer');
+  listOfProducts.forEach((product) => {
+    let productCard = createProductCard(product);
+    mainContainer.append(productCard);
+  });
 
-    listOfProducts.forEach((product) => {
-        let productCard = createProductCard(product)
-        mainContainer.append(productCard)
-        
-    })
-
-    let basket = document.querySelectorAll('.btnContainer');
-
-    
+  let basket = document.querySelectorAll(".btnContainer");
 
   /*   listOfProducts.push('inCart', 0); */
 
-    for (let i=0; i < basket.length; i++) {
-        basket[i].addEventListener('click', () => {
-            cartNumbers(listOfProducts[i]);
-            totalCost(listOfProducts[i])
-        })
+  for (let i = 0; i < basket.length; i++) {
+    basket[i].addEventListener("click", () => {
+      cartNumbers(listOfProducts[i]);
+      totalCost(listOfProducts[i]);
+    });
+  }
 
-       
+  // Add to cart function
+
+  function onLoadCartNumbers() {
+    let productNumber = localStorage.getItem("cartNumbers");
+
+    if (productNumber) {
+      document.querySelector(".itemsNumber, span").textContent = productNumber;
     }
+  }
 
-   // Add to cart function
+  function cartNumbers(product) {
+    let productNumber = localStorage.getItem("cartNumbers");
+    productNumber = parseInt(productNumber);
 
-    function onLoadCartNumbers() {
-        let productNumber = localStorage.getItem('cartNumbers');
-
-        if(productNumber) {
-            document.querySelector('.itemsNumber, span').textContent = productNumber;
-        }
+    if (productNumber) {
+      localStorage.setItem("cartNumbers", productNumber + 1);
+      document.querySelector(".itemsNumber, span").textContent =
+        productNumber + 1;
+    } else {
+      localStorage.setItem("cartNumbers", 1);
+      document.querySelector(".itemsNumber, span").textContent = 1;
     }
+    itemsInLocal(product);
+  }
 
-   function cartNumbers(product) {
-       
-       let productNumber = localStorage.getItem('cartNumbers');
-       productNumber = parseInt(productNumber);
-
-       if(productNumber) {
-        
-            localStorage.setItem('cartNumbers', productNumber + 1);
-            document.querySelector('.itemsNumber, span').textContent = productNumber + 1;
-        } else {
-            localStorage.setItem('cartNumbers', 1);
-            document.querySelector('.itemsNumber, span').textContent = 1;
-
-        }
-        itemsInLocal(product);
-       
-    }
-    
-    
-    onLoadCartNumbers();
+  onLoadCartNumbers();
 }
-   
+
 // shows items in cart
 function itemsInLocal(product) {
-    let getCartArray = localStorage.getItem('productsInCart');
-    getCartArray = JSON.parse(getCartArray);
+  let getCartArray = localStorage.getItem("productsInCart");
+  getCartArray = JSON.parse(getCartArray);
 
-    
-    if(!getCartArray) {
-        cartArray.push(product)
-    } else {
-        cartArray = getCartArray
-    }
+  if (!getCartArray) {
+    cartArray.push(product);
+  } else {
+    cartArray = getCartArray;
+  }
 
-    if(cartArray.some(item => item.title === product.title)) {
-        cartArray.map(item => {
-            if(item.title === product.title) {
-                !item.quantity ? item.quantity = 1 : item.quantity += 1 
-            }
-        })
-    }   else {
-        cartArray.push(product)
-        cartArray.map(item => {
-            if(item.title === product.title) {
-                !item.quantity ? item.quantity = 1 : item.quantity += 1
-            }
-        })
-    }
+  if (cartArray.some((item) => item.title === product.title)) {
+    cartArray.map((item) => {
+      if (item.title === product.title) {
+        !item.quantity ? (item.quantity = 1) : (item.quantity += 1);
+      }
+    });
+  } else {
+    cartArray.push(product);
+    cartArray.map((item) => {
+      if (item.title === product.title) {
+        !item.quantity ? (item.quantity = 1) : (item.quantity += 1);
+      }
+    });
+  }
 
-   localStorage.setItem('productsInCart', JSON.stringify (cartArray));
+  localStorage.setItem("productsInCart", JSON.stringify(cartArray));
 }
 
 // shows Total cost in Localstorage
-function totalCost(product){
-    let basketCost = localStorage.getItem('totalCost');
-    if(basketCost != null){
-        basketCost = parseInt(basketCost);
-        localStorage.setItem('totalCost', basketCost + product.price);
-    } else {
-        localStorage.setItem("totalCost", product.price);
-    }
+function totalCost(product) {
+  let basketCost = localStorage.getItem("totalCost");
+  if (basketCost != null) {
+    basketCost = parseInt(basketCost);
+    localStorage.setItem("totalCost", basketCost + product.price);
+  } else {
+    localStorage.setItem("totalCost", product.price);
+  }
 }
-
-
-
 
 function createProductCard(product) {
+  // create div-container for each phone
+  let productContainer = document.createElement("div");
+  productContainer.classList.add("productContainer");
 
-    // create div-container for each phone
-    let productContainer = document.createElement('div');
-    productContainer.classList.add('productContainer');
-    
+  //create img-container and IMG-tag
+  let imgContainer = document.createElement("div");
+  imgContainer.classList.add("imgContainer");
+  let productImg = document.createElement("img");
+  productImg.src = imgUrl + product.image;
+  imgContainer.appendChild(productImg);
 
-    //create img-container and IMG-tag
-    let imgContainer = document.createElement('div');
-    imgContainer.classList.add('imgContainer');
-    let productImg = document.createElement('img');   
-    productImg.src = imgUrl + product.image;
-    imgContainer.appendChild (productImg);
+  // create div-text descripton for each phone
+  let textContainer = document.createElement("div");
+  textContainer.classList.add("textContainer");
 
+  let productHeader = document.createElement("H1");
+  let productText = document.createElement("p");
 
-    // create div-text descripton for each phone
-    let textContainer = document.createElement('div');
-    textContainer.classList.add ('textContainer');
+  productHeader.innerText = product.title;
+  productText.innerText = product.description;
 
-    let productHeader = document.createElement('H1')
-    let productText = document.createElement('p');
-    
+  //create price div and p-tag
+  let priceContainer = document.createElement("div");
+  priceContainer.classList.add("priceContainer");
+  priceText = document.createElement("p");
+  priceContainer.appendChild(priceText);
+  priceText.innerText = product.price + " kr";
 
-    productHeader.innerText = product.title;
-    productText.innerText = product.description;
+  // create button div and tag
+  let btnContainer = document.createElement("div");
+  let cartIconContainer = document.createElement("div");
+  cartIconContainer.classList.add("cartIconContainer");
+  let addToCartBtn = document.createElement("div");
+  addToCartBtn.classList.add("cartBtn");
+  let btnText = document.createTextNode("Lägg till i kundvagnen");
+  btnContainer.classList.add("btnContainer");
 
+  let cartIcon = document.createElement("i");
+  cartIcon.classList.add("fas", "fa-cart-arrow-down");
 
+  // adding event listener to button
 
-    //create price div and p-tag
-    let priceContainer = document.createElement('div');
-    priceContainer.classList.add ('priceContainer');
-    priceText = document.createElement ('p');
-    priceContainer.appendChild (priceText);
-    priceText.innerText = product.price + ' kr';
+  btnContainer.append(cartIconContainer);
+  addToCartBtn.append(btnText);
+  btnContainer.appendChild(addToCartBtn);
+  cartIconContainer.appendChild(cartIcon);
 
-    // create button div and tag
-    let btnContainer = document.createElement('div');
-    let cartIconContainer = document.createElement('div')
-    cartIconContainer.classList.add('cartIconContainer')
-    let addToCartBtn = document.createElement('div');
-    addToCartBtn.classList.add ('cartBtn')
-    let btnText = document.createTextNode ('Lägg till i kundvagnen');
-    btnContainer.classList.add ('btnContainer');
-    
-    let cartIcon = document.createElement('i');
-    cartIcon.classList.add('fas', 'fa-cart-arrow-down');
+  // Appending everything ---->
+  productContainer.append(
+    textContainer,
+    imgContainer,
+    priceContainer,
+    btnContainer
+  );
+  textContainer.append(productHeader, productText);
 
-
-    // adding event listener to button
-    
-    
-    
-    
-    btnContainer.append (cartIconContainer);
-    addToCartBtn.append (btnText);
-    btnContainer.appendChild(addToCartBtn);
-    cartIconContainer.appendChild (cartIcon);
-    
-    // Appending everything ---->
-    productContainer.append(textContainer, imgContainer, priceContainer, btnContainer);
-    textContainer.append (productHeader, productText);
-
-    return productContainer
+  return productContainer;
 }
-
-
-
-
-
