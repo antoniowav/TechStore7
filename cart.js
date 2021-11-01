f4d26915399197c13fae2e0d6e68844d0bd3a8fa
Julius Djudjaku <juliusdjudjaku@gmail.com>
2021-11-01 09:55:16 +0100

    Style cart.css and add som JS to cart

--- cart.js
+++ cart.js
@@ -1,4 +1,4 @@
-const imgUrl = '/assets/';
+const imgUrl = '/assets';
 function initSite() {
     addProductsToWebpage();
     // This would also be a good place to initialize other parts of the UI
@@ -61,102 +61,50 @@ function addProductsToWebpage() {
         const cartSection = document.createElement('div');
         cartSection.classList.add('cartContainer')
         const quantityBox = document.createElement('div')
-        quantityBox.classList.add('quantityContainer')
+        quantityBox.classList.add('quantityContaier')
         quantityBox.append(quantityContainer)
         const totalPriceBox = document.createElement('div')
         totalPriceBox.classList.add('priceContainer')
         const totalPriceContainer = document.createElement('div')
         totalPriceContainer.classList.add('.price')
         totalPriceBox.append(totalPriceContainer)
-        const img = document.createElement('div')
-        img.classList.add('imgContainer')
-
-        // Skapar "slutför köp" knappen
-
-        const checkoutBtnFlex = document.createElement('div')
-        checkoutBtnFlex.classList.add('checkoutBtnFlex')
-        const checkOutBtnContainer = document.createElement('div');
-        checkOutBtnContainer.classList.add('checkOutBtnContainer');
-        const checkOutBtnText = document.createElement('span');
-        checkOutBtnText.innerText = ('Slutför ditt köp');
-        const checkoutBtnIcon = document.createElement('i');
-        checkoutBtnIcon.classList.add('fas', 'fa-check');
-
-        checkOutBtnContainer.append(checkoutBtnIcon, checkOutBtnText);
-        checkoutBtnFlex.append(checkOutBtnContainer);
+   
        
 
         
-        
+        quantityBox.append(`Antal: ${cartNumbers}`)
+        totalPriceBox.append(`Totalt pris: ${totalCost}` + 'kr')
+        mainContainer.append(cartSection, totalPriceBox, quantityBox);
         
         
         let products = localStorage.getItem('productsInCart')
-
-        // skapar cart-symbol
-        let cartIcon = document.createElement('i');
-        cartIcon.classList.add('fas', 'fa-shopping-cart');
-        
-        // Skapar Kundvagn-rubrik texten
-        let cartHeaderContainer = document.createElement('div');
-        cartHeaderContainer.classList.add('cartHeaderContainer')
-        let cartHeader = document.createElement('h1');
-        cartHeader.innerText=('Kundvagn');
-
-        cartHeaderContainer.append(cartIcon, cartHeader);
-        mainContainer.append(cartHeaderContainer);
-
-
-        
-        // Detta if-statement körs bara om kundvagen är TOM!
-        if (products == null){
-            let basketEmpty = document.createElement('h3');
-            basketEmpty.innerText=('Kundvagnen är tom!')
-
-            let basketEmptyContainer = document.createElement('div');
-            basketEmptyContainer.classList.add('message')
-
-            basketEmptyContainer.append(basketEmpty);
-            mainContainer.append(basketEmptyContainer); 
-
-       } else {
-
-        quantityBox.append(`Antal: ${cartNumbers}`)
-        totalPriceBox.append(`Totalt pris: ${totalCost} kr`)
-        mainContainer.append(cartSection, totalPriceBox, quantityBox, checkoutBtnFlex);
        
         products = JSON.parse(products)
         for (var i = 0; i < products.length; i++){
             const productCard = document.createElement('div');
-            const title = document.createElement('h1')
+            const title = document.createElement('h3')
             const imgDisp = document.createElement('img')
-            const imgContainer = document.createElement('div');
+            imgDisp.classList.add("phoneImg")
             imgDisp.setAttribute("src", `${imgUrl}/${products[i].image}`)
             imgDisp.setAttribute("alt", `${products[i].title}` )
-            const productPrice = document.createElement('h6')
-            const productQuantity = document.createElement('h5')
+            const productPrice = document.createElement('span')
+            const productQuantity = document.createElement('span')
+            const productInfo = document.createElement('div')
 
-
+            productInfo.classList.add('productInfo')
             productPrice.classList.add('productPrice')
             productQuantity.classList.add('productQuantity')
             productCard.classList.add('productCard')
-            imgContainer.classList.add('imgContainer')
-            
+            title.classList.add('productTitle')
 
-            
             productPrice.append(products[i].price + ' kr')
-            productQuantity.append('Antal: '+ products[i].quantity)
+            productQuantity.append('antal: '+ products[i].quantity)
             title.append(products[i].title)
             cartSection.appendChild(productCard);
-            imgContainer.append(imgDisp);
-            productCard.append( imgContainer, title, productPrice , productQuantity )
-
-        
-        
+            productCard.append( imgDisp)
+            productCard.appendChild(productInfo)
+            productInfo.append(title, productPrice, productQuantity)
         }
-
-    }
-
-       
 
         
        
